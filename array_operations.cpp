75dc6bceceefc13694773fd1d5f48cfa549f1284
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-02-17 00:24:38 +0530

    Create array_operations.cpp

---
+++ array_operations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++) {
+            if(nums[i]==nums[i+1]) {
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+
+        vector<int>ans;
+        int count=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]!=0) {
+                ans.push_back(nums[i]);
+                count++;
+            }
+        }
+
+        for(int i=0;i<nums.size()-count;i++) {
+            ans.push_back(0);
+        }
+
+        return ans;
+    }
+};
