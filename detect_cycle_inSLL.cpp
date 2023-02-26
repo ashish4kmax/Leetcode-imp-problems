1ea300fc2b69d43f1e31e3543ce2fb86bea39d45
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-02-26 13:32:51 +0530

    Create detect_cycle_inSLL.cpp

---
+++ detect_cycle_inSLL.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slowp = head;
+        ListNode* fastp = head;
+        while(fastp!=NULL && fastp->next!=NULL) {
+            slowp=slowp->next;
+            fastp=fastp->next->next;
+            if(slowp==fastp)
+                return true;
+        }
+
+        return false;
+    }
+};
