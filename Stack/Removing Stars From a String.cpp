040e52e19da5f6f17320f1aac3402fa0bd1adf71
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-04-11 15:16:06 +0530

    Update Removing Stars From a String.cpp

--- Stack/Removing Stars From a String.cpp
+++ Stack/Removing Stars From a String.cpp
@@ -16,3 +16,18 @@ public:
         return ans;
     }
 };
+
+
+/* Using two pointers 
+
+string removeStars(string s) {
+    int j = 0;
+    for (int i = 0; i < s.size(); ++i)
+        if (s[i] == '*')
+            j--;
+        else 
+            s[j++] = s[i];
+    return s.substr(0, j);
+}
+
+*/
