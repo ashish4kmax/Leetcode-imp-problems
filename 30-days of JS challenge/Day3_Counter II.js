d057d3a38af7c0e51206d83fb39dcc2d4ac25e13
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-05-07 10:17:20 +0530

    Update and rename Counter II.js to Day3_Counter II.js

--- 30-days of JS challenge/Day3_Counter II.js
+++ 30-days of JS challenge/Day3_Counter II.js
@@ -29,26 +29,58 @@ var createCounter = function(init) {
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
 // Optimized //
+//1.
 
-/*
 /**
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
+/*
 var createCounter = function(init) {
     let value=init;
 
     return {
-        increment:()=>++value,
+        increment:()=>++value,  // If our code just want a single line functionality!!
         decrement:()=>--value,
         reset:()=>value=init, 
     }
 };
-
+ */
 /**
  * const counter = createCounter(5)
  * counter.increment(); // 6
  * counter.reset(); // 5
  * counter.decrement(); // 4
  */
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+// One more way to do it declaring the object as a functiom//
+//2.
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+/*
+var createCounter = function(init) {
+    var counter = init; 
+    return {
+        // Returning an object as a function if you want more than one line of functionality!!.
+        increment: function() {
+            return ++counter;
+        },
+        decrement: function() {
+            return --counter;
+        },
+        reset: function() {
+            return counter=init;
+        }
+    }
+};
+*/
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
  */
