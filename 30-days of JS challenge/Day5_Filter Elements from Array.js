de436600aa70161a5ee6e494ca015273e41743d6
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-05-09 11:18:34 +0530

    Update Day5_Filter Elements from Array.js

--- 30-days of JS challenge/Day5_Filter Elements from Array.js
+++ 30-days of JS challenge/Day5_Filter Elements from Array.js
@@ -14,6 +14,8 @@ var filter = function(arr, fn) {
   
     // or else there is a one liner too that is :-
     // return arr.filter(fn);
-    /* But the reason we are not using this one liner is to more of understand what the problem is intended to do and write the full logic instead of simply using cut short method but 
+    /* 
+    But the reason we are not using this one liner is to more of understand what the problem is intended to do and write the full logic instead of simply using cut short method but 
     if you are well aware of these logics you can use the array.filter() function as well.
+    */
 };
