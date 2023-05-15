c8c08963a7c45304827ee85a11d370a12eb4174d
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-05-15 22:32:56 +0530

    Update Day9_Memoize.js

--- 30-days of JS challenge/Day9_Memoize.js
+++ 30-days of JS challenge/Day9_Memoize.js
@@ -1,53 +1,28 @@
 /**
-
  * @param {Function} fn
-
  */
 
 function memoize(fn) {
-
     var cache={}
-
     return function(...args) {
-
         var key = JSON.stringify(args);
-
         if(key in cache) {
-
             return cache[key];
-
         }
-
-        else {
-
-            
-
+        else {        
            var result = fn(...args);
-
            cache[key] = result;
-
            return result;
-
         }
-
     }
-
 }
 
 /** 
-
  * let callCount = 0;
-
  * const memoizedFn = memoize(function (a, b) {
-
  *	 callCount += 1; *   return a + b;
-
  * })
-
  * memoizedFn(2, 3) // 5
-
  * memoizedFn(2, 3) // 5
-
  * console.log(callCount) // 1 
-
  */
