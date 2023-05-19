d52e1880e473f2ed444752e0107bd5df60d9711f
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-05-19 12:40:08 +0530

    Create Day15_Debounce.js

---
+++ 30-days of JS challenge/Day15_Debounce.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    // Simple one liner using debounce function.
+    return _.debounce(fn,t);
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
