7b99badd0cf29369f9566cbd18a0ae8abbd723aa
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-06-08 00:26:06 +0530

    Update Group the People Given the Group Size They Belong To.cpp

--- Arrays/Group the People Given the Group Size They Belong To.cpp
+++ Arrays/Group the People Given the Group Size They Belong To.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& nums) {
         vector<vector<int>>res;
+        vector<int>temp;
         unordered_map<int,vector<int>>m;
         int n=nums.size();
         for(int i=0;i<n;i++) {
+            m[nums[i]].push_back(i);
             if(nums[i]==m[nums[i]].size()) {
                 res.push_back(m[nums[i]]);
-                m.erase(nums[i]);
+                m[nums[i]] = temp;
             }
-            m[nums[i]].push_back(i);
-        }
-
-        for(auto i:m) {
-           res.push_back(i.second);
         }
 
         return res;
