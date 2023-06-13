a74b106449d0a42e413484e022c24df60b805756
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-06-13 19:55:44 +0530

    Create Minimum Amount of Time to Collect Garbage.cpp

---
+++ Arrays/Minimum Amount of Time to Collect Garbage.cpp
@@ -0,0 +1,90 @@
+// This is naive solution there is a optimization related to prefix sum for this question try doing it!!.
+class Solution
+{
+public:
+    int garbageCollection(vector<string> &garbage, vector<int> &travel)
+    {
+        int mins = 0;
+        int n = garbage.size();
+        for (int i = 0; i < n; i++)
+        {
+            mins += garbage[i].length();
+        }
+
+        int countM = 0, countP = 0, countG = 0;
+        int P,G,M;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<garbage[i].length();j++) {
+                if(garbage[i][j]=='P') {
+                    P = i;
+                    goto end;
+                } 
+            }
+        }
+        end:
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < garbage[i].length(); j++) {
+                if (garbage[i][j] == 'G') {
+                    G = i;
+                    break;
+                }
+            }
+            if (G >= 0) {
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < garbage[i].length(); j++) {
+                if (garbage[i][j] == 'M') {
+                    M = i;
+                    break;
+                }
+            }
+            if (M >= 0) {
+                break;
+            }
+        }
+        
+        int cP=0,cG=0,cM=0;
+        if (P >= 0) {
+            if(P==0) P+=1;
+            else P=1;
+            for (int i = P; i < n; i++) {
+                countP += travel[i - 1];
+                if (garbage[i].find("P") < garbage[i].length()) {
+                    cP += countP;
+                    countP = 0;
+                }
+            }
+        }
+
+        if (G >= 0) {
+            if (G == 0) G += 1;
+            else G=1;
+            for (int i = G; i < n; i++) {
+                countG += travel[i - 1];
+                if (garbage[i].find("G") < garbage[i].length()) {
+                    cG += countG;
+                    countG = 0;
+                }
+            }
+        }
+
+        if (M >= 0) {
+            if (M == 0) M += 1;
+            else M=1;
+            for (int i = 1; i < n; i++) {
+                countM += travel[i - 1];
+                if (garbage[i].find("M") < garbage[i].length()) {
+                    cM += countM;
+                    countM = 0;
+                }
+            }
+        }
+
+        mins+=cG+cP+cM;
+        return mins;
+    }
+};
