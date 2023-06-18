f285c3366f9eaaa407719ec6fbeb546bea505fde
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-06-18 22:14:15 +0530

    Create Finding the Users Active Minutes.cpp

---
+++ Hash Table/Finding the Users Active Minutes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        vector<int>ans (k,0);
+        vector<int>uams;
+        unordered_map<int,set<int>>m;
+        unordered_map<int,int>mp;
+        int n = logs.size();
+        for(int i=0;i<n;i++) {
+            m[logs[i][0]].insert(logs[i][1]);
+        }
+
+        for(auto i:m) {
+            uams.push_back(i.second.size());
+        }
+
+        for(int i=0;i<uams.size();i++) {
+            mp[uams[i]]++;
+        }
+
+        for(auto i:mp) {
+            ans[i.first-1] = i.second;
+        }
+
+        return ans;
+    }
+};
