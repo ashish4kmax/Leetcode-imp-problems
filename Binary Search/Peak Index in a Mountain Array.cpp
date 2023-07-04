ca4d4ff8a3e007de609da55f7caf055acc9cd8ba
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-07-05 00:51:09 +0530

    Update Peak Index in a Mountain Array.cpp

--- Binary Search/Peak Index in a Mountain Array.cpp
+++ Binary Search/Peak Index in a Mountain Array.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+        int start=0;
+        int end=arr.size()-1;
+        
+        while(start<end)
+        {
+           int mid=start+(end-start)/2; // To avoid overflows.
+           if(arr[mid]<arr[mid+1])
+            {
+                start=mid+1;
+            }
+            if(arr[mid]>arr[mid+1])
+            {
+                end=mid;
+            }
+        }
+        return start;
+        
+    }
+};
+
+/*
+Linear approch O(n)
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
         int n = arr.size();
         for(int i=0;i<n-1;i++) {
             if(arr[i]>arr[i+1]) return i;
@@ -8,3 +36,4 @@ public:
         return -1;
     }
 };
+*/
