a9e1a1b46f33da083709cff700cd08e3a7f3d719
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-07-10 21:57:05 +0530

    Update Minimum Depth of Binary Tree.cpp

--- Trees/Minimum Depth of Binary Tree.cpp
+++ Trees/Minimum Depth of Binary Tree.cpp
@@ -36,3 +36,54 @@ public:
         return min(l , r) + 1;
     }
 };
+
+// Using BFS (Breadth First Search Algorithm)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/*
+class Solution {
+public:
+    // using bfs
+    int minDepth(TreeNode* root) {
+        ios_base::sync_with_stdio(false);
+        if (root == NULL)
+            return 0;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+        
+        while (!q.empty()) {
+            int size = q.size();
+            depth++;
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if (node->left == NULL && node->right == NULL)
+                    return depth;
+                
+                if (node->left)
+                    q.push(node->left);
+                
+                if (node->right)
+                    q.push(node->right);
+            }
+        }
+        
+        return 0; // In case the tree is empty
+    }
+};
+*/
