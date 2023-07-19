7820822dba39f86e062c95c5013de67c61a96b43
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-07-19 23:49:55 +0530

    Create Maximize Sum Of Array After K Negations.cpp

---
+++ Sorting/Maximize Sum Of Array After K Negations.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+
+        int i = 0;
+        int op = 0;
+
+        while(i!=n-1 && op<k && nums[i]<0) {
+            if(nums[i]<0) {
+                nums[i] = abs(nums[i]);
+                i++;
+                op++;
+            }
+        }   
+
+        int min = 0;
+        for(int i=0;i<n;i++) {
+            if(nums[i]<nums[min]) min = i;
+        }
+
+        //cout<<nums[min]<<" ";
+
+        while(op<k) {
+            if(nums[min]<0) {
+                nums[min] = abs(nums[min]);
+                op++;
+            }
+            else if(nums[min]==0) {
+                op++;
+            }
+            else {
+                nums[min] = -nums[min];
+                op++;
+            }
+        }
+
+        int res = 0;
+
+        for(int i=0;i<n;i++) {
+            res+=nums[i];
+        }
+
+        return res;
+    }
+};
