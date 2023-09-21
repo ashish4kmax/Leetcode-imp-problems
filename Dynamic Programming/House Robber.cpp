8a3d22467c7c250bbd4caa6d00491f78c72892f2
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-09-21 08:12:50 +0530

    Update House Robber.cpp

--- Dynamic Programming/House Robber.cpp
+++ Dynamic Programming/House Robber.cpp
@@ -1,3 +1,10 @@
+// very very important problem it's an optimization problem try to use a and b then prevs to solve this
+/*
+TC:-O(n)
+SC:-O(1)
+
+Basically its like taking idx = 0 and idx = 1 as a and b and calculating the values and updating the maximum till we reach last index updating the value of a and b at each even for a and odd for b idx.
+*/
 class Solution {
 public:
     int rob(vector<int>& arr) {
