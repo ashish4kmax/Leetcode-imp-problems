12d6dd4c0ab8c37c7ddb37efe3100c96c7026ece
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-09-22 10:39:14 +0530

    Create Is Subsequence.cpp

---
+++ Dynamic Programming/Is Subsequence.cpp
@@ -0,0 +1,37 @@
+// TC: O(n^2) TC can be imporved till O(nlogn) max maybe O(n) too.
+// SC: O(n)
+class Solution {
+public:
+    bool isSubsequence(string t, string s) {
+        map<char,vector<int>>m;
+        int n = s.length();
+
+        for(int i=0;i<n;i++) {
+            m[s[i]].push_back(i);
+        }
+
+        int prev = -1;
+        for(int i=0;i<t.length();i++) {
+            if(m[t[i]].size()==0) return false;
+            else {
+                if(prev==-1) {
+                    prev = m[t[i]][0];
+                }
+                else {
+                    int found = -1;
+                    for(int j=0;j<m[t[i]].size();j++) {
+                        if(m[t[i]][j]>prev) {
+                            found = m[t[i]][j];
+                            break;
+                        }
+                    }
+
+                    if(found<prev) return false;
+                    else prev = found;
+                }
+            }
+        }
+
+        return true;
+    }
+};
