7b92dca9546bf6fb0c2720f7fc5fb8d2a47d5036
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-11-02 11:08:16 +0530

    Update Merge Intervals.cpp

--- Sorting/Merge Intervals.cpp
+++ Sorting/Merge Intervals.cpp
@@ -1,52 +1,27 @@
-// Efficient O(N*N) algorithm if sorted intervals are passed then can be done in O(N).
+// Efficient O(N*logN) algorithm if sorted intervals are passed then can be done in O(N).
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>res;
+        
+        int mini = intervals[0][0];
+        int maxi = intervals[0][1];
         int n = intervals.size();
 
-        vector<vector<int>>res;
-        vector<pair<int,int>>inter;
-        
-        for(int i=0;i<n;i++) {
-            pair<int,int>temp;
-            temp.first = intervals[i][0];
-            temp.second = intervals[i][1];
-            inter.push_back(temp);
-        }
-
-        sort(inter.begin(),inter.end());
-
-        vector<int>temp;
-        int mini= inter[0].first, maxi=inter[0].second;
-        for(int i=1;i<n;i++) {
-            if(inter[i].first>=mini && inter[i].first<=maxi) {
-               maxi = max(maxi,inter[i].second);
+        for(int i=0;i<n-1;i++) {
+            if(maxi>=intervals[i+1][0] && maxi<=intervals[i+1][1]) {
+                maxi = max(maxi, intervals[i+1][1]);
             }
             else {
-                temp.push_back(mini);
-                temp.push_back(maxi);
-                res.push_back(temp);
-                mini = inter[i].first;
-                maxi = inter[i].second;
+                if(maxi>=intervals[i+1][0] && maxi>=intervals[i+1][1]) continue;
+                res.push_back({mini,maxi});
+                mini = intervals[i+1][0];
+                maxi = intervals[i+1][1];
             }
-            temp.clear();
         }
 
-        if(inter[n-1].first>=mini && inter[n-1].first<=maxi) {
-            maxi = max(maxi,inter[n-1].second);
-        }
-        else {
-            mini = inter[n-1].first;
-            maxi = inter[n-1].second;
-        }
-
-
-        temp.push_back(mini);
-        temp.push_back(maxi);
-        res.push_back(temp);
+        res.push_back({mini,maxi});
 
         return res;
     }
